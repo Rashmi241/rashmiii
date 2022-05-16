c84b1705324e32226dc004b210357783da6aaf23
Rashmi <98954270+Rashmi241@users.noreply.github.com>
2022-05-16 14:30:34 +0530

    edited

--- 4b.sh
+++ 4b.sh
@@ -3,22 +3,13 @@
 #begin. If this second argument is not present, the search is to begin in the current working
 #directory. In either case, the starting directory as well as its subdirectories at all levels must be
 #searched. The script need not include error checking.
-if [ $# -eq 3 ]
+if [ "$2"!=" " ]
 then
-if [ -f $1 ]
-then
-l =` cat $1 | wc -l`
-if [ $2 -gt $3 -a $2 -gt $1 a $3 -gt $2 ]
-then
-head -n $1 $3 | tail -n +$2
+cwd='pwd'
+cd =$2
+link=`ls -l $1 | tr -s " " -f 2`
+cd $cwd
 else
-echo "enter valid argument"
+link=`ls -l $1 | tr -s " " -f 2`
 fi
-else
-echo "file doesnt exist"
-fi
-else
-echo "entre the nunber of argument"
-fi
-
-    
+echo "no of links of file $1:$link"
